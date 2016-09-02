615952424a9301fdac6eb22e874c6f378bc5747d
Esther Leytush <eleytush@gmail.com>
2016-09-02 02:07:46 -0400

    tabbing, cleanup

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 var path;
 
 function distance(lat1, lon1, lat2, lon2) {
-  var radlat1 = Math.PI * lat1/180;
-  var radlat2 = Math.PI * lat2/180;
-  var radlon1 = Math.PI * lon1/180;
-  var radlon2 = Math.PI * lon2/180;
-  var theta = lon1-lon2;
-  var radtheta = Math.PI * theta/180;
-  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  dist = Math.acos(dist);
-  dist = dist * 180/Math.PI;
-  dist = dist * 60 * 1.1515;
-  // here, dist is turned into nautical miles
-  dist = dist * 0.8684;
-  return dist;
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var radlon1 = Math.PI * lon1/180;
+    var radlon2 = Math.PI * lon2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    // here, dist is turned into nautical miles
+    dist = dist * 0.8684;
+    return dist;
 }
 
 function showNauticalDistance(bothLocations) {
@@ -27,19 +27,22 @@ function showNauticalDistance(bothLocations) {
     $("#distance").html(Math.round(nauticalDistance * 100) / 100);
 
     var flightPlanCoordinates = [
-      {lat: lat1, lng: lng1},
-      {lat: lat2, lng: lng2},
+        {lat: lat1, lng: lng1},
+        {lat: lat2, lng: lng2},
     ];
+
     flightPath = new google.maps.Polyline({
-      path: flightPlanCoordinates,
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
     });
+
     if (path) {
-      path.setMap(null);
+        path.setMap(null);
     }
+
     path = flightPath;
     path.setMap(map);
 };
@@ -53,18 +56,18 @@ function initMap() {
   var bounds = new google.maps.LatLngBounds();
 
   var options = {
-    //  types: ['establishment'], // establishments only
-     componentRestrictions: {country: 'us'} //USA only
+      //  types: ['establishment'], // establishments only
+       componentRestrictions: {country: 'us'} //USA only
   };
 
   var markerFrom = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
   });
 
   var markerTo = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
   });
 
   var inputFrom = (document.getElementById('pac-input-from'));
@@ -77,88 +80,87 @@ function initMap() {
   var autocompleteTo = new google.maps.places.Autocomplete(inputTo, options);
 
   var bothLocations = {
-    'firstLocation': undefined,
-    'secondLocation': undefined
+      'firstLocation': undefined,
+      'secondLocation': undefined
   }
 
   var infowindowFrom = new google.maps.InfoWindow();
   autocompleteFrom.addListener('place_changed', function() {
-    infowindowFrom.close();
-    markerFrom.setVisible(false);
+      infowindowFrom.close();
+      markerFrom.setVisible(false);
 
-    var placeFrom = autocompleteFrom.getPlace();
-    if (!placeFrom.geometry) {
-      window.alert("Please check your input! I couldn't find that place!");
-      return;
-    } else {
-      bothLocations.firstLocation = { lat: placeFrom.geometry.location.lat(), lng: placeFrom.geometry.location.lng() };
+      var placeFrom = autocompleteFrom.getPlace();
+      if (!placeFrom.geometry) {
+          window.alert("Please check your input! I couldn't find that place!");
+          return;
+      } else {
+          bothLocations.firstLocation = { lat: placeFrom.geometry.location.lat(), lng: placeFrom.geometry.location.lng() };
 
-      LatLngList[0] = new google.maps.LatLng (placeFrom.geometry.location.lat(), placeFrom.geometry.location.lng());
-      for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
-        bounds.extend(LatLngList[i]);
+          LatLngList[0] = new google.maps.LatLng (placeFrom.geometry.location.lat(), placeFrom.geometry.location.lng());
+          for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
+              bounds.extend(LatLngList[i]);
+          }
+          map.fitBounds(bounds);
+
+          if (bothLocations.firstLocation && bothLocations.secondLocation) {
+              showNauticalDistance(bothLocations);
+          }
       }
-      map.fitBounds(bounds);
 
-      if (bothLocations.firstLocation && bothLocations.secondLocation) {
-        showNauticalDistance(bothLocations);
+      // Drop marker with nice label
+      markerFrom.setPosition(placeFrom.geometry.location);
+      markerFrom.setVisible(true);
+
+      var addressFrom = '';
+      if (placeFrom.address_components) {
+          addressFrom = [
+              (placeFrom.address_components[0] && placeFrom.address_components[0].short_name || ''),
+              (placeFrom.address_components[1] && placeFrom.address_components[1].short_name || ''),
+              (placeFrom.address_components[2] && placeFrom.address_components[2].short_name || '')
+          ].join(' ');
       }
-    }
 
-    // Drop marker with nice label
-    markerFrom.setPosition(placeFrom.geometry.location);
-    markerFrom.setVisible(true);
-
-    var addressFrom = '';
-    if (placeFrom.address_components) {
-      addressFrom = [
-        (placeFrom.address_components[0] && placeFrom.address_components[0].short_name || ''),
-        (placeFrom.address_components[1] && placeFrom.address_components[1].short_name || ''),
-        (placeFrom.address_components[2] && placeFrom.address_components[2].short_name || '')
-      ].join(' ');
-    }
-
-    infowindowFrom.setContent('<div><strong>' + placeFrom.name + '</strong><br>' + addressFrom);
-    infowindowFrom.open(map, markerFrom);
+      infowindowFrom.setContent('<div><strong>' + placeFrom.name + '</strong><br>' + addressFrom);
+      infowindowFrom.open(map, markerFrom);
   });
 
   var infowindowTo = new google.maps.InfoWindow();
   autocompleteTo.addListener('place_changed', function() {
-    infowindowTo.close();
-    bounds[1] = markerTo;
-    markerTo.setVisible(false);
+      infowindowTo.close();
+      markerTo.setVisible(false);
 
-    var placeTo = autocompleteTo.getPlace();
-    if (!placeTo.geometry) {
-      window.alert("Please check your input! I couldn't find that place!");
-      return;
-    } else {
-      bothLocations.secondLocation = { lat: placeTo.geometry.location.lat(), lng: placeTo.geometry.location.lng() };
+      var placeTo = autocompleteTo.getPlace();
+      if (!placeTo.geometry) {
+          window.alert("Please check your input! I couldn't find that place!");
+          return;
+      } else {
+          bothLocations.secondLocation = { lat: placeTo.geometry.location.lat(), lng: placeTo.geometry.location.lng() };
 
-      LatLngList[1] = new google.maps.LatLng (placeTo.geometry.location.lat(), placeTo.geometry.location.lng());
-      for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
-        bounds.extend(LatLngList[i]);
+          LatLngList[1] = new google.maps.LatLng (placeTo.geometry.location.lat(), placeTo.geometry.location.lng());
+          for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
+              bounds.extend(LatLngList[i]);
+          }
+          map.fitBounds(bounds);
+
+          if (bothLocations.firstLocation && bothLocations.secondLocation) {
+              showNauticalDistance(bothLocations);
+          }
       }
-      map.fitBounds(bounds);
 
-      if (bothLocations.firstLocation && bothLocations.secondLocation) {
-        showNauticalDistance(bothLocations);
+      markerTo.setPosition(placeTo.geometry.location);
+      markerTo.setVisible(true);
+
+      var addressTo = '';
+      if (placeTo.address_components) {
+          addressTo = [
+              (placeTo.address_components[0] && placeTo.address_components[0].short_name || ''),
+              (placeTo.address_components[1] && placeTo.address_components[1].short_name || ''),
+              (placeTo.address_components[2] && placeTo.address_components[2].short_name || '')
+          ].join(' ');
       }
-    }
 
-    markerTo.setPosition(placeTo.geometry.location);
-    markerTo.setVisible(true);
-
-    var addressTo = '';
-    if (placeTo.address_components) {
-      addressTo = [
-        (placeTo.address_components[0] && placeTo.address_components[0].short_name || ''),
-        (placeTo.address_components[1] && placeTo.address_components[1].short_name || ''),
-        (placeTo.address_components[2] && placeTo.address_components[2].short_name || '')
-      ].join(' ');
-    }
-
-    infowindowTo.setContent('<div><strong>' + placeTo.name + '</strong><br>' + addressTo);
-    infowindowTo.open(map, markerTo);
+      infowindowTo.setContent('<div><strong>' + placeTo.name + '</strong><br>' + addressTo);
+      infowindowTo.open(map, markerTo);
   });
 
 }
